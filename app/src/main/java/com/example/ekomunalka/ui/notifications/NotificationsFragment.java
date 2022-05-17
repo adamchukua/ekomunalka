5d85fa4256c748f7eb62a3b34a543146bb912ccb
thegradle <adamchuk.v.1@gmail.com>
2022-05-17 21:36:32 +0300

    Add test notification

--- app/src/main/java/com/example/ekomunalka/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/ekomunalka/ui/notifications/NotificationsFragment.java
@@ -1,20 +1,38 @@
 package com.example.ekomunalka.ui.notifications;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.ekomunalka.MainActivity;
+import com.example.ekomunalka.R;
 import com.example.ekomunalka.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    private NotificationManager notificationManager;
+    private Button notifyBtn;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        notificationManager = (NotificationManager) getActivity().getSystemService(Context
+                .NOTIFICATION_SERVICE);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,6 +45,31 @@ public class NotificationsFragment extends Fragment {
         //final TextView textView = binding.textNotifications;
         //notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getActivity().getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        notifyBtn = (Button) view.findViewById(R.id.button);
+        notifyBtn.setOnClickListener(view1 -> {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(), "My Notification")
+                    .setContentTitle("Тестове повідомлення")
+                    .setContentText("Привіт! Дякую що натиснув на цю кнопочку, ось тобі тестовий текст повідомлення")
+                    .setSmallIcon(R.drawable.ic_stat_logo)
+                    .setAutoCancel(true)
+                    .setStyle(new NotificationCompat.BigTextStyle()
+                            .bigText("Привіт! Дякую що натиснув на цю кнопочку, ось тобі тестовий текст повідомлення"));
+
+            NotificationManagerCompat managerCompat = NotificationManagerCompat.from(getActivity());
+            managerCompat.notify(1, builder.build());
+        });
     }
 
     @Override
