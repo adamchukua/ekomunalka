c5a8d8a98630b5ccdf9fe0ab0caa422a5f6199ba
thegradle <adamchuk.v.1@gmail.com>
2022-05-17 14:29:51 +0300

    Fix full screen

--- app/src/main/java/com/example/ekomunalka/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ekomunalka/ui/home/HomeFragment.java
@@ -24,7 +24,6 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        requireActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
 
         //final TextView textView = binding.textHome;
