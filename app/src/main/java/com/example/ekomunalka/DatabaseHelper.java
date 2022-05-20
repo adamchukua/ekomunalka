1adad7863da0c372536c2dce3a37867043cf04b4
thegradle <adamchuk.v.1@gmail.com>
2022-05-20 16:05:12 +0300

    Refactoring

--- app/src/main/java/com/example/ekomunalka/DatabaseHelper.java
+++ app/src/main/java/com/example/ekomunalka/DatabaseHelper.java
@@ -36,16 +36,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         long result = db.insert(TABLE_NAME, null, contentValues);
 
-        //if date as inserted incorrectly it will return -1
-        if (result == -1) {
-            return false;
-        }
-
-        return true;
+        return result != -1;
     }
     public Cursor getListContents() {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
-        return data;
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
     }
 }
