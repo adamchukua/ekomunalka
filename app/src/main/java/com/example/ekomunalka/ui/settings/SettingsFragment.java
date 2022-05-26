39afd9bd10d51346d2ce8239120a24f3b5f5a3cb
thegradle <adamchuk.v.1@gmail.com>
2022-05-26 12:07:04 +0300

    Add settingsList to SettingsFragment

--- app/src/main/java/com/example/ekomunalka/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/ekomunalka/ui/settings/SettingsFragment.java
@@ -1,14 +1,11 @@
 package com.example.ekomunalka.ui.settings;
 
-import android.database.Cursor;
-import android.hardware.lights.LightState;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.SimpleCursorAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -29,8 +26,10 @@ public class SettingsFragment extends Fragment {
     private FragmentSettingsBinding binding;
     private DatabaseHelper db;
     private MainActivity mainActivity;
+    private ListView info;
     private ListView settings;
-    private SimpleAdapter adapter;
+    private SimpleAdapter infoAdapter;
+    private SimpleAdapter settingsAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -43,16 +42,27 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        String[][] settingsItems =
+        String[][] infoItems =
                 {{"Версія", getActivity().getResources().getString(R.string.version)},
                 {"Розробник", "@thegradle"}};
+        String[][] settingsItems =
+                {{"Тарифи", "Додати, змінити чи видалити тарифи"}};
 
         db = new DatabaseHelper(getContext());
         mainActivity = new MainActivity();
+
+        info = view.findViewById(R.id.informationList);
         settings = view.findViewById(R.id.settingsList);
 
+        List<Map<String, String>> infoList = new ArrayList<>();
         List<Map<String, String>> settingsList = new ArrayList<>();
 
+        for (String[] infoItem : infoItems) {
+            Map<String, String> item = new HashMap<>();
+            item.put("title", infoItem[0]);
+            item.put("subtitle", infoItem[1]);
+            infoList.add(item);
+        }
         for (String[] settingsItem : settingsItems) {
             Map<String, String> item = new HashMap<>();
             item.put("title", settingsItem[0]);
@@ -60,7 +70,14 @@ public class SettingsFragment extends Fragment {
             settingsList.add(item);
         }
 
-        adapter = new SimpleAdapter(
+        infoAdapter = new SimpleAdapter(
+                getActivity(),
+                infoList,
+                R.layout.mylist1,
+                new String[] { "title", "subtitle" },
+                new int[] { R.id.title, R.id.subtitle }
+        );
+        settingsAdapter = new SimpleAdapter(
                 getActivity(),
                 settingsList,
                 R.layout.mylist1,
@@ -68,7 +85,8 @@ public class SettingsFragment extends Fragment {
                 new int[] { R.id.title, R.id.subtitle }
         );
 
-        settings.setAdapter(adapter);
+        info.setAdapter(infoAdapter);
+        settings.setAdapter(settingsAdapter);
     }
 
     @Override
